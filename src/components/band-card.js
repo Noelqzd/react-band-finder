7b085b4a34db11ba0a10532178fd694a68f21f94
Noel Quezada <noelquezada@gmail.com>
2019-12-07 19:19:56 -0800

    fix styles

--- src/components/band-card.js
+++ src/components/band-card.js
@@ -5,20 +5,33 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Hidden from '@material-ui/core/Hidden';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles(theme => ({
+    card: {
+        display: 'flex',
 
+    },
+    cardDetails: {
+        flex: 1,
+    },
+    cardMedia: {
+        width: 160,
+    }
+}))
 
 const BandCard = ({ band }) => {
+    const classes = useStyles();
     return (
 
         <Grid item key={band.id} xs={12} md={6}>
             <CardActionArea component="a" href="#">
-                <Card>
-                    <div >
+                <Card className={classes.card}>
+                    <div className={classes.cardDetails}>
                         <CardContent>
                             <Typography component="h2" variant="h5">
                                 {band.name}
+
                             </Typography>
                             <Typography variant="subtitle1" color="textSecondary">
                                 {band.genre ? band.genre.name : "Genre Undefined"}
@@ -28,16 +41,17 @@ const BandCard = ({ band }) => {
                             </Typography>
                             <Typography variant="subtitle1" color="primary">
                                 More Info...
+
                         </Typography>
                         </CardContent>
                     </div>
-                    <Hidden xsDown>
-                        <CardMedia
 
-                            image={band.imgUrl}
-                            title="Image title"
-                        />
-                    </Hidden>
+                    <CardMedia
+                        className={classes.cardMedia}
+                        image={band.imgUrl}
+                        title="Image title"
+                    />
+
                 </Card>
             </CardActionArea>
         </Grid>
